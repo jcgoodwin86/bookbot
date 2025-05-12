443c4e559f912ecd5e16e40ef6b6dd9d3ac0edad
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2025-05-11 21:40:04 -0400

    chore: refactor `main` to use command-line arguments for input file
    - Add `sys` import for command-line argument handling
    - Implement command-line argument check for input file
    - Update `book_path` to use command-line argument
    - Add `print_report` function for structured output
    - Modify `main` to call `print_report` with sorted data
    - Introduce `get_report` function to sort character data
    - Add `sort_on` helper function for sorting dictionary

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from stats import get_word_count, get_char_count
+from stats import get_word_count, get_char_count, get_report
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     book = get_book_text(book_path)
     book_word_count = get_word_count(book)
     char_count = get_char_count(book)
+    sorted_data = get_report(char_count)
+    print_report(book_path, book_word_count, sorted_data)
 
-    print(f'{book_word_count} words found in the document')
-    print(char_count)
+def print_report(book_path, book_word_count, sorted_data):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f'Found {book_word_count} total words')
+    print("--------- Character Count -------")
+    for letter in sorted_data:
+        print(f"{letter["char"]}: {letter["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,17 @@ def get_char_count(book):
             char_dict[lowered] = 1
 
     return char_dict
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_report(data):
+    report_dict = []
+
+    for letter in data:
+        if letter.isalpha():
+            report_dict.append({"char": letter, "num": data[letter]})
+
+    report_dict.sort(reverse=True, key=sort_on)
+
+    return report_dict
